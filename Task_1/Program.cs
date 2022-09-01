05c5c1110ee01bb1a9691d479e30d5a1e1bdb480
Olya <abrosimovaon@yandex.ru>
2022-09-01 20:02:51 +0400

    Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите A: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите B: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if(number1 > number2)
+{
+    Console.WriteLine("A - большее число, B - меньшее число");
+}
+else
+
+{
+    Console.WriteLine("A - меньшее число, B - большее число");
+}
