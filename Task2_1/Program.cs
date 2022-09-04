53a8a18d83b65b7b774174f46314576b61fdf10a
Olya <abrosimovaon@yandex.ru>
2022-09-04 18:54:53 +0400

    Task2_1

---
+++ Task2_1/Program.cs
@@ -0,0 +1,8 @@
+﻿ //Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+
+System.Console.WriteLine("Введите трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a / 10;
+int c = b%10;
+System.Console.WriteLine(c);
+
