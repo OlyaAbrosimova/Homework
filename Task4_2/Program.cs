87b9622bc54c83f06fcd86f65c4b22daeb4ae960
Olya <abrosimovaon@yandex.ru>
2022-09-13 23:42:39 +0400

    изменение файла

--- Task4_2/Program.cs
+++ Task4_2/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+Console.Write("Введите число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+  int Sum(int number)
+  {
+    int counter = Convert.ToString(number).Length;
+    int number1 = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++)
+    {
+     number1 = number - number % 10;
+      result = result + (number - number1);
+      number = number / 10;
+    }
+   return result;
+  }
+
+int sum = Sum(number);
+Console.WriteLine(sum);
